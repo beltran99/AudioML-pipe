06c8946f3244b5299bcf930323dc9d47ee285efb
beltran99 <beltran.castro2@gmail.com>
2025-11-01 12:57:10 +0100

    update logging format

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,16 @@
 import logging
 
-def get_logger(level=logging.DEBUG):
-    return logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%d/%m/%Y %I:%M:%S %p',
-        level=level,
-    )
+def get_logger(name: str = __name__, level=logging.DEBUG) -> logging.Logger:
+    """Create or get a logger configured with consistent formatting."""
+    logger = logging.getLogger(name)
+    if not logger.handlers: # avoid duplicates
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%d/%m/%Y %I:%M:%S %p',
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(level)
+    
+    return logger
