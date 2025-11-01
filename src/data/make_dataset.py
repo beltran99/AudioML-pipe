42402697636a847ed2d2ac3d34d5a31324a501f8
beltran99 <beltran.castro2@gmail.com>
2025-11-01 12:57:28 +0100

    added logging

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,18 +3,23 @@ import argparse
 import librosa
 import logging
 import os
+import sys
 import numpy as np
+from src.logger import get_logger
 
 ROOT_DIR = Path(__file__).parent / '../..' # project root
+logging_level = logging.DEBUG
 
 def process_raw_data(raw_data, sample_rate, n_mfcc):
 
     extracted_features = librosa.feature.mfcc(y=raw_data, sr=sample_rate, n_mfcc=n_mfcc)
-    extracted_features = np.mean(extracted_features.T, axis=0)
+    extracted_features = np.mean(extracted_features.T, axis=0)  # aggregate over t (time)
     
     return extracted_features
 
 def load_data(data_path, n_mfcc):
+    
+    logger = get_logger(__name__, logging_level)
     
     dataset = []    # tuples of (input_features, label)
     
@@ -22,53 +27,95 @@ def load_data(data_path, n_mfcc):
     filepaths = [
         Path(os.path.join(root, file))
         for root, dirs, files in os.walk(str(data_path)) if not dirs # take only leaf nodes
-        for file in files 
+        for file in files
     ]
+    if not filepaths:
+        logger.warning(f"Could not find any files in specified path {data_path}")
+        
+    n_files = len(filepaths)
+    progress = int(n_files * 0.2) # Log progress in 20% completion increments
+    count = 0
     for filepath in filepaths:
         # storing the speaker info may be useful if we wanted to
         # split the data in a specific way to avoid imbalance
         speaker, filename = filepath.parent.name, filepath.stem
         # label is expected to be at the beggining of filename, followed by underscore
         label = int(filename.split("_")[0])
+        # reading files one by one seems inefficient,
+        # there might be an efficient way of batching them
         raw_data, sample_rate = read_file(filepath)
         extracted_features = process_raw_data(raw_data, sample_rate, n_mfcc)
         
         dataset.append((extracted_features, label))
         
+        count +=1
+        if count % progress == 0:
+            pctg = (count / n_files)*100
+            logger.info(f"Current progress: {pctg:.2f}%. Processed [{count}/{n_files}] audio files.")
+            
+    logger.info("Finished processing audio files.")
+        
     return dataset
 
 def read_file(filepath):
+    
+    logger = get_logger(__name__, logging_level)
+    
     assert filepath.is_file()
     try:
         raw_data, sample_rate = librosa.load(filepath)
+    except FileNotFoundError as err:
+        logger.error(f"Audio file not found in the specified path {filepath}\n\tError details: {err=}, {type(err)=}")
+        sys.exit(1)
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        logger.error(f"Unexpected error when trying to read audio file\n\tError details: {err=}, {type(err)=}")
+        sys.exit(1)
     
     return raw_data, sample_rate
 
-def save_dataset(dataset, name='mydataset'):
+def save_dataset(dataset, name):
+    
+    logger = get_logger(__name__, logging_level)
+
+    if not dataset:
+        logger.warning(f"Dataset {name} is empty. Cannot save an empty dataset.")
+        return
+        
     new_dataset_path = ROOT_DIR / ("data/processed/" + name + ".npz")
     x, y = zip(*dataset)
 
     try:
         np.savez(new_dataset_path, x=x, y=y)
+        logger.info(f"Dataset succesfully saved to {new_dataset_path}.")
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        logger.error(f"Unexpected error when trying to save processed data\n\tError details: {err=}, {type(err)=}")
+        sys.exit(1)
 
+def main(path: Path, n_mfcc: int, name: str):
+    
+    logger = get_logger(__name__, logging_level)
+    
+    # 1. Process raw
+    logger.info(f"Processing raw data from {path}")
+    dataset = load_data(path, n_mfcc)
+    
+    # 2. Save processed data for future use
+    logger.info(f"Saving processed dataset {name}")
+    save_dataset(dataset, name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process raw input data.')
     parser.add_argument('--path', '-p', type=str, default= ROOT_DIR / ("data/raw/") , help='Path to raw data. Please specify path from project root.')
     parser.add_argument('--mfcc', type=int, default=40, help='Number of Mel-frequency cepstrum coefficients to extract from the input data.')
+    parser.add_argument('--name', type=str, default="mydataset", help='Dataset name.')
     parser.add_argument('--verbose', '-v', action="store_true", help='Log informational messages.')
     args = parser.parse_args()
     
     path = args.path
     n_mfcc = args.mfcc
+    name = args.name
     
-    logging_level = logging.DEBUG
     if not args.verbose:
-        logging_level = logging.CRITICAL
+        logging_level = logging.ERROR
         
-    dataset = load_data(path, n_mfcc)
-    save_dataset(dataset)
+    main(path, n_mfcc, name)
